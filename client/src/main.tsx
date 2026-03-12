ca620f3dda6dd0f51ecdc7d333941bb2f2b1d65e
Leonardo Teodoro <lauteodor@gmail.com>
2026-03-11 22:48:41 -0300

    chore: add google tag manager tracking

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,7 +3,26 @@ import App from "./App";
 import { redirectLegacyHashIfNeeded } from "./lib/legacyHashRedirect";
 import "./index.css";
 
+declare global {
+  interface Window {
+    dataLayer?: Array<Record<string, string>>;
+  }
+}
+
+function trackPageView() {
+  const hashPath = window.location.hash.replace(/^#/, "") || "/";
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({
+    event: "virtual_page_view",
+    page_title: document.title,
+    page_path: hashPath,
+    page_location: `${window.location.origin}${window.location.pathname}#${hashPath === "/" ? "/" : hashPath}`,
+  });
+}
+
 redirectLegacyHashIfNeeded();
 window.addEventListener("hashchange", redirectLegacyHashIfNeeded);
+window.addEventListener("hashchange", trackPageView);
 
 createRoot(document.getElementById("root")!).render(<App />);
+trackPageView();
